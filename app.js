4507936a148dd64f37dc4e99dad1de2ee6e1bfb6
Evan Genest <genest.1@osu.edu>
2018-12-24 07:44:45 -0600

    Require in a module, 'messin'
    Create module: 'messinWithThePage.js'.
    The module imports a function called paneTwo.confirm.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { artistsString } from './data/artists.js';
 //let artistsString = import('./data/artists.js');
 let artists = JSON.parse(artistsString);
+import { paneTwo } from './assets/messinWithThePage.js';
 /*//let techsString = import("./data/techs.js");
 //let techs = JSON.parse(techsString);
 let leadersString = import('./data/leaders.js');
@@ -8,9 +9,9 @@ let leaders = JSON.parse(leadersString);
 let shakespearesString = import('./data/shakespeares.js');
 let shakespeares = JSON.parse(shakespearesString);
 */
-console.log(`What? Imported thingie is ${ artists.dylan}`);
 console.log(`The size of the loaded DYLAN, is ${artists.dylan.length}.`);
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
+document.addEventListener(`DOMContentLoaded`, function(event) {
+    console.log(`DOM fully loaded and parsed`);
+    console.log(paneTwo.confirm());
 /*    document.write('<h3>Make me smile.   Make me smile.<\h3>');
 */  });
---
+++ assets/messinWithThePage.js
@@ -0,0 +1,9 @@
+let paneTwo = {};
+
+paneTwo.confirm = ()=> {
+	const NOW = new Date;
+	return `Madam, I am present. ${NOW}`;
+}; 
+
+export {paneTwo};
+
