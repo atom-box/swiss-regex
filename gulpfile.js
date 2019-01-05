93ea0a6a310c6cac3da001208c082851b3686cc7
Evan Genest <genest.1@osu.edu>
2019-01-05 16:22:42 -0600

    Make a pipe that modifies any saving of .css.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,18 @@ let gulp = require('gulp'), watch = require('gulp-watch');
 
 gulp.task('default', function(){console.log`yipee yahoo wow.`});
 gulp.task('html',  ()=>{console.log("Rent the tandem..");});
+gulp.task('styles',  ()=>{
+	console.log("Imagine STYLE tasks running here.");
+	return gulp.src('./styles/figFlood.css').pipe(gulp.dest('./styles/temp.css'));
+	;
+	// see Udemy 4--12 or tutorialNotes
+});
+
+
+
 gulp.task('watch', function() {
 	watch('./index.html', ()=>{
 		gulp.start('html');
 	});
+	watch('./styles/**/*.css', ()=>{gulp.start('styles')}
 });
