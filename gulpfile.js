2bc5d0fe05a77eb5cbeac35149740cf2d7cb6be6
Evan Genest <genest.1@osu.edu>
2019-01-05 16:02:59 -0600

    Started gulpfile, moved css to assets.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-let gulp = require('gulp');
+let gulp = require('gulp'), watch = require('gulp-watch');
+
+gulp.task('default', function(){console.log`yipee yahoo wow.`});
+gulp.task('html',  ()=>{console.log("Rent the tandem..");});
+gulp.task('watch', function() {
+	watch('./index.html', ()=>{
+		gulp.start('html');
+	});
+});
