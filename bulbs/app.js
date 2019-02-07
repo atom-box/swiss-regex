f41a8021ccb8063d0460fa1195afa159816aed42
Evan Genest <genest.1@osu.edu>
2019-02-06 21:54:41 -0600

    Start a 2nd page: Bulb visualizer.

---
+++ bulbs/app.js
@@ -0,0 +1,36 @@
+import { artistsString } from './data/artists.js';
+//let artistsString = import('./data/artists.js');
+let artists = JSON.parse(artistsString);
+import { paneTwo } from './assets/messinWithThePage.js';
+/*//let techsString = import("./data/techs.js");
+//let techs = JSON.parse(techsString);
+let leadersString = import('./data/leaders.js');
+let leaders = JSON.parse(leadersString);
+let shakespearesString = import('./data/shakespeares.js');
+let shakespeares = JSON.parse(shakespearesString);
+*/
+console.log(`The size of the loaded DYLAN, is ${artists.dylan.length}.`);
+
+
+var btn1 = document.getElementById('color-button');
+function random(number) {
+  return Math.floor(Math.random()*(number+1));
+}
+btn1.onclick = function() {
+  var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
+
+
+let btn2 = document.getElementById('go-button');
+btn2.onclick = paneTwo.showResults;
+
+
+
+document.addEventListener(`DOMContentLoaded`, function(event) {
+    console.log(`DOM fully loaded and parsed`);
+    console.log(paneTwo.confirm());
+    console.log(paneTwo.showResults());
+    console.log(`In ${paneTwo.listWindowKeys()}`);
+/*    document.write('<h3>Make me smile.   Make me smile.<\h3>');
+*/  });
